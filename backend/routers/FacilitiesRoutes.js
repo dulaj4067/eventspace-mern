6e2c510cbc117656a8490acbfc94ef5ca9aca652
Dewmina <it23598928@my.sliit.lk>
2026-02-24 09:08:21 +0530

    Added improved security measures

--- backend/routers/FacilitiesRoutes.js
+++ backend/routers/FacilitiesRoutes.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const facilityController = require("../controllers/Facilitiescontroller");
+const facilityController = require("../controllers/FacilitiesController");
 
-router.post("/", facilityController.createFacility);
-router.post("/bulk", facilityController.createFacilitiesBulk);
+const { protect, authorize } = require("../middlewares/authMiddleware");
 
+// Public routes
 router.get("/", facilityController.getAllFacilities);
 router.get("/:id", facilityController.getFacilityById);
 
-router.put("/:id", facilityController.updateFacility);
-router.put("/bulk/update", facilityController.updateFacilitiesBulk);
+// Admin-only bulk routes
+router.post("/bulk",protect,authorize("admin"),facilityController.createFacilitiesBulk);
 
-router.delete("/:id", facilityController.deleteFacility);
-router.delete("/bulk/delete", facilityController.deleteFacilitiesBulk);
+router.put("/bulk/update",protect,authorize("admin"),facilityController.updateFacilitiesBulk);
+
+router.delete("/bulk/delete",protect,authorize("admin"),facilityController.deleteFacilitiesBulk);
+
+
+router.post("/", protect, authorize("admin"), facilityController.createFacility);
+router.put("/:id", protect, authorize("admin"), facilityController.updateFacility);
+router.delete("/:id", protect, authorize("admin"), facilityController.deleteFacility);
 
 module.exports = router;
